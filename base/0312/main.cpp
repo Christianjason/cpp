a7092a0d7bcb91a989cb605dddcb5eace2708670
赵杰 <chengduzhaojie@yeah.net>
2024-03-12 23:44:44 +0800

    提交基础目录

---
+++ base/0312/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 赵杰 on 2024/3/12.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
---
+++ base/0312/main.h
@@ -0,0 +1,9 @@
+//
+// Created by 赵杰 on 2024/3/12.
+//
+
+#ifndef CPP_GIT_MAIN_H
+#define CPP_GIT_MAIN_H
+
+
+#endif //CPP_GIT_MAIN_H
